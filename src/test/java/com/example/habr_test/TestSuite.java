cec9eb540307df2926661f351652a17feab41647
wedemeda <gagarin.rama@gmail.com>
2023-11-03 20:36:43 +0100

    initial commit
    Добавлен проект тестирования формы регистрации на сайте habr.com

---
+++ src/test/java/com/example/habr_test/TestSuite.java
@@ -0,0 +1,12 @@
+package com.example.habr_test;
+
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.DisplayNameGeneration;
+import org.junit.platform.suite.api.SelectPackages;
+import org.junit.platform.suite.api.Suite;
+
+@Suite
+@SelectPackages("com.example.habr_test")
+public class TestSuite {
+
+}
